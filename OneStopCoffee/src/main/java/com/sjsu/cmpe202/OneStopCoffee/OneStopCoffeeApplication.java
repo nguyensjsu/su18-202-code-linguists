f8a5f92a9b985365bb00ad8ce55d6b0e054ba94c
Barkha <barkha.choithani@sjsu.edu>
2018-07-18 21:28:19 -0700

    initial spring boot setup

---
+++ OneStopCoffee/src/main/java/com/sjsu/cmpe202/OneStopCoffee/OneStopCoffeeApplication.java
@@ -0,0 +1,12 @@
+package com.sjsu.cmpe202.OneStopCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneStopCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneStopCoffeeApplication.class, args);
+	}
+}
---
+++ OneStopCoffee/src/test/java/com/sjsu/cmpe202/OneStopCoffee/OneStopCoffeeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sjsu.cmpe202.OneStopCoffee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OneStopCoffeeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
